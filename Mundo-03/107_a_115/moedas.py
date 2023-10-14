b1a804df59ecde49eaad54877b830c8846a24d8c
Bruno Haag <proarkes@gmail.com>
2023-10-14 20:47:16 -0300

    inicio da modularização

---
+++ Mundo-03/107_a_115/moedas.py
@@ -0,0 +1,15 @@
+def metade(moedas):
+    return moedas / 2
+
+
+def dobro(moedas):
+    return moedas*2
+
+
+def aumentar(moedas):
+    return moedas*1.1
+
+
+def diminuir(moedas):
+    d = moedas * 0.13
+    return moedas - d
